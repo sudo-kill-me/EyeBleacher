99fa125658d2d22be919267ed4e126e08a644c5c
Ryan <31938131+rytrotter@users.noreply.github.com>
2019-04-02 17:13:04 -0400

    Add files via upload
    Cleaned up some code: Got rid of duplicate individual Subreddit data classes by making the parsable json data it's own class. I also created a method to pull the data rather than call the same code each time.

--- EyeBleacher/Form1.Designer.cs
+++ EyeBleacher/Form1.Designer.cs
@@ -32,14 +32,21 @@ namespace EyeBleacher
             this.cuteButton = new System.Windows.Forms.Button();
             this.wholesomeButton = new System.Windows.Forms.Button();
             this.coolButton = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.usernamePromptTextBox = new System.Windows.Forms.TextBox();
-            this.usernameTextBox = new System.Windows.Forms.TextBox();
-            this.titlePromptTextBox = new System.Windows.Forms.TextBox();
             this.titleTextBox = new System.Windows.Forms.TextBox();
-            this.subredditPromptTextBox = new System.Windows.Forms.TextBox();
-            this.subredditTextBox = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.subredditPromptLabel = new System.Windows.Forms.Label();
+            this.usernamePromptLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subredditLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.upvotesLabel = new System.Windows.Forms.Label();
+            this.downvotesLabel = new System.Windows.Forms.Label();
+            this.downvoteImage = new System.Windows.Forms.PictureBox();
+            this.upvoteImage = new System.Windows.Forms.PictureBox();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.openUrlButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.downvoteImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upvoteImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // cuteButton
@@ -51,105 +58,154 @@ namespace EyeBleacher
             this.cuteButton.TabIndex = 2;
             this.cuteButton.Text = "Something Cute";
             this.cuteButton.UseVisualStyleBackColor = true;
-            this.cuteButton.Click += new System.EventHandler(this.cuteButton_Click);
+            this.cuteButton.Click += new System.EventHandler(this.CuteButton_Click);
             // 
             // wholesomeButton
             // 
-            this.wholesomeButton.Location = new System.Drawing.Point(280, 508);
+            this.wholesomeButton.Location = new System.Drawing.Point(335, 508);
             this.wholesomeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.wholesomeButton.Name = "wholesomeButton";
             this.wholesomeButton.Size = new System.Drawing.Size(185, 34);
             this.wholesomeButton.TabIndex = 4;
             this.wholesomeButton.Text = "Something Wholesome";
             this.wholesomeButton.UseVisualStyleBackColor = true;
-            this.wholesomeButton.Click += new System.EventHandler(this.wholesomeButton_Click);
+            this.wholesomeButton.Click += new System.EventHandler(this.WholesomeButton_Click);
             // 
             // coolButton
             // 
-            this.coolButton.Location = new System.Drawing.Point(585, 508);
+            this.coolButton.Location = new System.Drawing.Point(668, 508);
             this.coolButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.coolButton.Name = "coolButton";
             this.coolButton.Size = new System.Drawing.Size(185, 34);
             this.coolButton.TabIndex = 5;
             this.coolButton.Text = "Something Cool";
             this.coolButton.UseVisualStyleBackColor = true;
-            this.coolButton.Click += new System.EventHandler(this.coolButton_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 95);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(758, 408);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            // 
-            // usernamePromptTextBox
-            // 
-            this.usernamePromptTextBox.Cursor = System.Windows.Forms.Cursors.No;
-            this.usernamePromptTextBox.Location = new System.Drawing.Point(12, 39);
-            this.usernamePromptTextBox.Name = "usernamePromptTextBox";
-            this.usernamePromptTextBox.ReadOnly = true;
-            this.usernamePromptTextBox.Size = new System.Drawing.Size(75, 22);
-            this.usernamePromptTextBox.TabIndex = 10;
-            this.usernamePromptTextBox.Text = "Username:";
-            // 
-            // usernameTextBox
-            // 
-            this.usernameTextBox.Location = new System.Drawing.Point(93, 39);
-            this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.ReadOnly = true;
-            this.usernameTextBox.Size = new System.Drawing.Size(167, 22);
-            this.usernameTextBox.TabIndex = 8;
-            // 
-            // titlePromptTextBox
-            // 
-            this.titlePromptTextBox.Cursor = System.Windows.Forms.Cursors.No;
-            this.titlePromptTextBox.Location = new System.Drawing.Point(12, 67);
-            this.titlePromptTextBox.Name = "titlePromptTextBox";
-            this.titlePromptTextBox.ReadOnly = true;
-            this.titlePromptTextBox.Size = new System.Drawing.Size(75, 22);
-            this.titlePromptTextBox.TabIndex = 10;
-            this.titlePromptTextBox.Text = "Title:";
+            this.coolButton.Click += new System.EventHandler(this.CoolButton_Click);
             // 
             // titleTextBox
             // 
             this.titleTextBox.Location = new System.Drawing.Point(93, 67);
             this.titleTextBox.Name = "titleTextBox";
             this.titleTextBox.ReadOnly = true;
-            this.titleTextBox.Size = new System.Drawing.Size(677, 22);
+            this.titleTextBox.Size = new System.Drawing.Size(760, 22);
             this.titleTextBox.TabIndex = 11;
             // 
-            // subredditPromptTextBox
+            // subredditPromptLabel
             // 
-            this.subredditPromptTextBox.Cursor = System.Windows.Forms.Cursors.No;
-            this.subredditPromptTextBox.Location = new System.Drawing.Point(11, 11);
-            this.subredditPromptTextBox.Name = "subredditPromptTextBox";
-            this.subredditPromptTextBox.ReadOnly = true;
-            this.subredditPromptTextBox.Size = new System.Drawing.Size(75, 22);
-            this.subredditPromptTextBox.TabIndex = 10;
-            this.subredditPromptTextBox.Text = "Subreddit:";
+            this.subredditPromptLabel.AutoSize = true;
+            this.subredditPromptLabel.Location = new System.Drawing.Point(12, 14);
+            this.subredditPromptLabel.Name = "subredditPromptLabel";
+            this.subredditPromptLabel.Size = new System.Drawing.Size(73, 17);
+            this.subredditPromptLabel.TabIndex = 14;
+            this.subredditPromptLabel.Text = "Subreddit:";
             // 
-            // subredditTextBox
+            // usernamePromptLabel
             // 
-            this.subredditTextBox.Location = new System.Drawing.Point(93, 11);
-            this.subredditTextBox.Name = "subredditTextBox";
-            this.subredditTextBox.ReadOnly = true;
-            this.subredditTextBox.Size = new System.Drawing.Size(167, 22);
-            this.subredditTextBox.TabIndex = 13;
+            this.usernamePromptLabel.AutoSize = true;
+            this.usernamePromptLabel.Location = new System.Drawing.Point(12, 42);
+            this.usernamePromptLabel.Name = "usernamePromptLabel";
+            this.usernamePromptLabel.Size = new System.Drawing.Size(81, 17);
+            this.usernamePromptLabel.TabIndex = 15;
+            this.usernamePromptLabel.Text = "Username: ";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(12, 70);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(43, 17);
+            this.titleLabel.TabIndex = 16;
+            this.titleLabel.Text = "Title: ";
+            // 
+            // subredditLabel
+            // 
+            this.subredditLabel.AutoSize = true;
+            this.subredditLabel.Location = new System.Drawing.Point(99, 14);
+            this.subredditLabel.Name = "subredditLabel";
+            this.subredditLabel.Size = new System.Drawing.Size(0, 17);
+            this.subredditLabel.TabIndex = 17;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(99, 42);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(0, 17);
+            this.usernameLabel.TabIndex = 18;
+            // 
+            // upvotesLabel
+            // 
+            this.upvotesLabel.AutoSize = true;
+            this.upvotesLabel.Location = new System.Drawing.Point(332, 14);
+            this.upvotesLabel.Name = "upvotesLabel";
+            this.upvotesLabel.Size = new System.Drawing.Size(0, 17);
+            this.upvotesLabel.TabIndex = 19;
+            // 
+            // downvotesLabel
+            // 
+            this.downvotesLabel.AutoSize = true;
+            this.downvotesLabel.Location = new System.Drawing.Point(332, 42);
+            this.downvotesLabel.Name = "downvotesLabel";
+            this.downvotesLabel.Size = new System.Drawing.Size(0, 17);
+            this.downvotesLabel.TabIndex = 20;
+            // 
+            // downvoteImage
+            // 
+            this.downvoteImage.Image = global::EyeBleacher.Properties.Resources.downvoteLogo;
+            this.downvoteImage.Location = new System.Drawing.Point(311, 42);
+            this.downvoteImage.Name = "downvoteImage";
+            this.downvoteImage.Size = new System.Drawing.Size(15, 15);
+            this.downvoteImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.downvoteImage.TabIndex = 22;
+            this.downvoteImage.TabStop = false;
+            // 
+            // upvoteImage
+            // 
+            this.upvoteImage.Image = global::EyeBleacher.Properties.Resources.upvoteLogo;
+            this.upvoteImage.Location = new System.Drawing.Point(311, 16);
+            this.upvoteImage.Name = "upvoteImage";
+            this.upvoteImage.Size = new System.Drawing.Size(15, 15);
+            this.upvoteImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.upvoteImage.TabIndex = 21;
+            this.upvoteImage.TabStop = false;
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Location = new System.Drawing.Point(12, 95);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(841, 408);
+            this.mainPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mainPictureBox.TabIndex = 6;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // openUrlButton
+            // 
+            this.openUrlButton.BackColor = System.Drawing.SystemColors.Control;
+            this.openUrlButton.Location = new System.Drawing.Point(11, 95);
+            this.openUrlButton.Name = "openUrlButton";
+            this.openUrlButton.Size = new System.Drawing.Size(126, 32);
+            this.openUrlButton.TabIndex = 23;
+            this.openUrlButton.Text = "Open in Browser";
+            this.openUrlButton.UseVisualStyleBackColor = false;
+            this.openUrlButton.Click += new System.EventHandler(this.OpenUrlButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 553);
-            this.Controls.Add(this.subredditTextBox);
-            this.Controls.Add(this.subredditPromptTextBox);
+            this.ClientSize = new System.Drawing.Size(865, 553);
+            this.Controls.Add(this.openUrlButton);
+            this.Controls.Add(this.downvoteImage);
+            this.Controls.Add(this.upvoteImage);
+            this.Controls.Add(this.downvotesLabel);
+            this.Controls.Add(this.upvotesLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.subredditLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.usernamePromptLabel);
+            this.Controls.Add(this.subredditPromptLabel);
             this.Controls.Add(this.titleTextBox);
-            this.Controls.Add(this.titlePromptTextBox);
-            this.Controls.Add(this.usernameTextBox);
-            this.Controls.Add(this.usernamePromptTextBox);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.mainPictureBox);
             this.Controls.Add(this.coolButton);
             this.Controls.Add(this.wholesomeButton);
             this.Controls.Add(this.cuteButton);
@@ -157,7 +213,9 @@ namespace EyeBleacher
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "EyeBleacher";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downvoteImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upvoteImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,13 +225,18 @@ namespace EyeBleacher
         private System.Windows.Forms.Button cuteButton;
         private System.Windows.Forms.Button wholesomeButton;
         private System.Windows.Forms.Button coolButton;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox usernamePromptTextBox;
-        private System.Windows.Forms.TextBox usernameTextBox;
-        private System.Windows.Forms.TextBox titlePromptTextBox;
+        private System.Windows.Forms.PictureBox mainPictureBox;
         private System.Windows.Forms.TextBox titleTextBox;
-        private System.Windows.Forms.TextBox subredditPromptTextBox;
-        private System.Windows.Forms.TextBox subredditTextBox;
+        private System.Windows.Forms.Label subredditPromptLabel;
+        private System.Windows.Forms.Label usernamePromptLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subredditLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label upvotesLabel;
+        private System.Windows.Forms.Label downvotesLabel;
+        private System.Windows.Forms.PictureBox upvoteImage;
+        private System.Windows.Forms.PictureBox downvoteImage;
+        private System.Windows.Forms.Button openUrlButton;
     }
 }
 
---
+++ EyeBleacher/ParsableRedditJsonData.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+
+namespace EyeBleacher
+{
+    class ParsableRedditJsonData
+    {
+
+        public class Data2
+        {
+            public object approved_at_utc { get; set; }
+            public string subreddit { get; set; }
+            public string selftext { get; set; }
+            public string author_fullname { get; set; }
+            public bool saved { get; set; }
+            public object mod_reason_title { get; set; }
+            public float gilded { get; set; }
+            public bool clicked { get; set; }
+            public string title { get; set; }
+            public List<object> link_flair_richtext { get; set; }
+            public string subreddit_name_prefixed { get; set; }
+            public bool hidden { get; set; }
+            public float pwls { get; set; }
+            public object link_flair_css_class { get; set; }
+            public float downs { get; set; }
+            public float? thumbnail_height { get; set; }
+            public bool hide_score { get; set; }
+            public string name { get; set; }
+            public bool quarantine { get; set; }
+            public string link_flair_text_color { get; set; }
+            public object author_flair_background_color { get; set; }
+            public string subreddit_type { get; set; }
+            public float ups { get; set; }
+            public string domain { get; set; }
+            public float? thumbnail_width { get; set; }
+            public object author_flair_template_id { get; set; }
+            public bool is_original_content { get; set; }
+            public List<object> user_reports { get; set; }
+            public bool is_reddit_media_domain { get; set; }
+            public bool is_meta { get; set; }
+            public object category { get; set; }
+            public object link_flair_text { get; set; }
+            public bool can_mod_post { get; set; }
+            public float score { get; set; }
+            public object approved_by { get; set; }
+            public string thumbnail { get; set; }
+            public bool edited { get; set; }
+            public object author_flair_css_class { get; set; }
+            public List<object> author_flair_richtext { get; set; }
+            public object content_categories { get; set; }
+            public bool is_self { get; set; }
+            public object mod_note { get; set; }
+            public float created { get; set; }
+            public string link_flair_type { get; set; }
+            public float wls { get; set; }
+            public object banned_by { get; set; }
+            public string author_flair_type { get; set; }
+            public bool contest_mode { get; set; }
+            public object selftext_html { get; set; }
+            public object likes { get; set; }
+            public object suggested_sort { get; set; }
+            public object banned_at_utc { get; set; }
+            public object view_count { get; set; }
+            public bool archived { get; set; }
+            public bool no_follow { get; set; }
+            public bool is_crosspostable { get; set; }
+            public bool pinned { get; set; }
+            public bool over_18 { get; set; }
+            public bool media_only { get; set; }
+            public bool can_gild { get; set; }
+            public bool spoiler { get; set; }
+            public bool locked { get; set; }
+            public object author_flair_text { get; set; }
+            public bool visited { get; set; }
+            public object num_reports { get; set; }
+            public object distinguished { get; set; }
+            public string subreddit_id { get; set; }
+            public object mod_reason_by { get; set; }
+            public object removal_reason { get; set; }
+            public string link_flair_background_color { get; set; }
+            public string id { get; set; }
+            public bool is_robot_indexable { get; set; }
+            public object report_reasons { get; set; }
+            public string author { get; set; }
+            public float num_crossposts { get; set; }
+            public float num_comments { get; set; }
+            public bool send_replies { get; set; }
+            public string whitelist_status { get; set; }
+            public List<object> mod_reports { get; set; }
+            public bool author_patreon_flair { get; set; }
+            public object author_flair_text_color { get; set; }
+            public string permalink { get; set; }
+            public string parent_whitelist_status { get; set; }
+            public string url { get; set; }
+
+        }
+
+        public class Child
+        {
+            public string kind { get; set; }
+            public Data2 data { get; set; }
+        }
+
+        public class Data
+        {
+            public string modhash { get; set; }
+            public float dist { get; set; }
+            public List<Child> children { get; set; }
+            public string after { get; set; }
+            public object before { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string kind { get; set; }
+            public Data data { get; set; }
+        }
+    }
+}
